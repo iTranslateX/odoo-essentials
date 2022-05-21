bd44ced0ad13fba025707d54b0ca780c58f4c628
alanhou <info@iscbj.com>
2022-05-21 18:58:45 +0800

    第五章 Odoo 15开发之导入、导出以及模块数据

---
+++ source-code/ch05/library_app/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Library Management",
+    "summary": "Manage library catalog and book lending.",
+    "author": "Alan Hou",
+    "license": "AGPL-3",
+    "category": "Services/Library",
+    "website": "https://alanhou.org",
+    "version": "15.0.1.0.0",
+    "depends": ["base"],
+    "data": [
+        "security/library_security.xml",
+        "security/ir.model.access.csv",
+        "views/book_view.xml",
+        "views/library_menu.xml",
+        "views/book_list_template.xml",
+    ],
+    "demo": [
+        "data/res.partner.csv",
+        "data/library.book.csv",
+        "data/book_demo.xml",
+    ],
+    "application": True,
+}
